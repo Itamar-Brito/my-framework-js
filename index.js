312c50ca649620bd7a661ceda7879c8e3c613443
Itamar-Brito <itamar.brito@gmail.com>
2023-02-20 21:50:50 -0300

    ola mundo renan

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Olá, mundo!1' });
+  res.json({ message: 'Olá, mundo! RENAN TU É O CARA' });
 });
 
 app.listen(3001, () => {
-  console.log('Servidor iniciado na porta 3000');
+  console.log('Servidor iniciado na porta 3001');
 });
