acb5733bc52456efada87248165c50d7bf222ab0
Itamar-Brito <itamar.brito@gmail.com>
2023-02-20 21:41:16 -0300

    firstCommit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/teste', (req, res) => {
+  res.json({ message: 'Olá, mundo!1' });
+});
+
+app.listen(3000, () => {
+  console.log('Servidor iniciado na porta 3000');
+});
